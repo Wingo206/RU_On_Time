b3590455506279903c79b7cafa3bb60c3a006b0e
ALIENHZ1 <hgz5@scarletmail.rutgers.edu>
2021-11-19 13:16:12 -0500

    navigation bar
    Added navigation bar at the bottom and new pages for testing.

--- lib/page/main.dart
+++ lib/page/main.dart
@@ -47,6 +47,11 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
+  final screens = [
+    Center(child: Text('Calendar', style: TextStyle(fontSize: 60))),
+    Center(child: Text('Assignments', style: TextStyle(fontSize: 60))),
+    Center(child: Text('Leaderboard', style: TextStyle(fontSize: 60))),
+  ];
 
   void _incrementCounter() {
     setState(() {
@@ -67,6 +72,7 @@ class _MyHomePageState extends State<MyHomePage> {
     // The Flutter framework has been optimized to make rerunning build methods
     // fast, so that you can just rebuild anything that needs updating rather
     // than having to individually change instances of widgets.
+    int currentIndex = 0;
     return Scaffold(
       appBar: AppBar(
         // Here we take the value from the MyHomePage object that was created by
@@ -76,6 +82,7 @@ class _MyHomePageState extends State<MyHomePage> {
       body: Center(
         // Center is a layout widget. It takes a single child and positions it
         // in the middle of the parent.
+
         child: Column(
           // Column is also a layout widget. It takes a list of children and
           // arranges them vertically. By default, it sizes itself to fit its
@@ -120,6 +127,31 @@ class _MyHomePageState extends State<MyHomePage> {
           ],
         ),
       ),
+
+      bottomNavigationBar: BottomNavigationBar(
+        type: BottomNavigationBarType.fixed,
+        currentIndex: currentIndex,
+        onTap: (index) => setState(() => currentIndex = index),
+        iconSize: 50,
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.calendar_today_rounded),
+            label: 'Calendar',
+            backgroundColor: Colors.blue,
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.assignment_rounded),
+            label: 'Submissions',
+            backgroundColor: Colors.blue,
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.leaderboard_rounded),
+            label: 'Leaderboard',
+            backgroundColor: Colors.blue,
+          ),
+        ],
+      ),
+
       floatingActionButton: FloatingActionButton(
         onPressed: _incrementCounter,
         tooltip: 'Increment',
