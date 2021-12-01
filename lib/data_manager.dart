d0da337b7fd963e48ef702ded8f3742e14276fb7
Wingo206 <50254747+Wingo206@users.noreply.github.com>
2021-11-30 19:47:27 -0500

    using UserInfo class

--- lib/data_manager.dart
+++ lib/data_manager.dart
@@ -4,13 +4,16 @@ import 'package:firebase_auth/firebase_auth.dart';
 class DataManager {
   String uid;
 
-  CollectionReference usersCollection = FirebaseFirestore.instance.collection('users');
+  CollectionReference usersCollection =
+      FirebaseFirestore.instance.collection('users');
 
   DocumentReference get userRef => usersCollection.doc(uid);
 
-  CollectionReference get assignmentCollection => usersCollection.doc(uid).collection('assignments');
+  CollectionReference get assignmentCollection =>
+      usersCollection.doc(uid).collection('assignments');
 
-  Stream<QuerySnapshot> get assignmentStream => assignmentCollection.orderBy('due date', descending: false).snapshots();
+  Stream<QuerySnapshot> get assignmentStream =>
+      assignmentCollection.orderBy('due date', descending: false).snapshots();
 
   DataManager(this.uid);
 
@@ -22,16 +25,28 @@ class DataManager {
 
     await manager.userRef.get().then((DocumentSnapshot snapshot) {
       if (snapshot.exists) {
-        
       } else {
-        manager.userRef.set({
-          'coins': 0,
-          'gems': 0,
-        });
+        manager.userRef.set(
+          UserInfo(
+            name: "unset",
+            email: _firebaseAuth.currentUser!.email!,
+            coins: 0,
+            gems: 0,
+            xp: 0,
+          ).toJson(),
+        );
       }
     });
 
     return manager;
+  }
+
+  Future<UserInfo> getUserInfo() {
+    return userRef.get().then(
+      (DocumentSnapshot snapshot) {
+        return UserInfo.fromJson(snapshot.data()! as Map<String, dynamic>, uid);
+      },
+    );
   }
 }
 
@@ -41,17 +56,26 @@ class UserInfo {
   final int coins;
   final int gems;
   final int xp;
+  String? documentID;
 
-  UserInfo({required this.name, required this.email, required this.coins, required this.gems, required this.xp});
+  UserInfo({
+    required this.name,
+    required this.email,
+    required this.coins,
+    required this.gems,
+    required this.xp,
+    this.documentID,
+  });
 
   UserInfo.fromJson(Map<String, Object?> json, String id)
       : this(
-    name: json['name']! as String,
-    email: json['email']! as String,
-    coins: json['coins']! as int,
-    gems: json['gems']! as int,
-    xp: json['xp']! as int,
-  );
+          name: json['name']! as String,
+          email: json['email']! as String,
+          coins: json['coins']! as int,
+          gems: json['gems']! as int,
+          xp: json['xp']! as int,
+          documentID: id,
+        );
 
   Map<String, Object?> toJson() {
     return {
